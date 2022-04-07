281d7cfbb22a9d1f6bed35b0c6acfbe5146ecbb7
Szymon Szczyrbak <szymon.szczyrbak@op.pl>
2022-04-07 22:39:02 +0200

    Start implementing glTF shading model.

--- src/vulkan/objects/shader_generator.c
+++ src/vulkan/objects/shader_generator.c
@@ -89,6 +89,12 @@ void glsl_add_vertex_shader_output_variables(UT_string *s) {
   location++;
   utstring_printf(s, "layout(location = %u) out vec3 outColor;\n", location);
   location++;
+  utstring_printf(s, "layout(location = %u) out vec3 outWorldPosition;\n", location);
+  location++;
+  utstring_printf(s, "#ifdef IN_NORMAL\n");
+  utstring_printf(s, "layout(location = %u) out vec3 outWorldNormal;\n", location);
+  utstring_printf(s, "#endif\n");
+  location++;
   utstring_printf(s, "#ifdef IN_TEXCOORD\n");
   utstring_printf(s, "layout(location = %u) out vec2 outTexCoord;\n", location);
   utstring_printf(s, "#endif\n");
@@ -99,6 +105,12 @@ void glsl_add_fragment_shader_input_variables(UT_string *s) {
   utstring_printf(s, "layout(location = %u) in flat uint inInstanceId;\n", location);
   location++;
   utstring_printf(s, "layout(location = %u) in vec3 inColor;\n", location);
+  location++;
+  utstring_printf(s, "layout(location = %u) in vec3 inWorldPosition;\n", location);
+  location++;
+  utstring_printf(s, "#ifdef IN_NORMAL\n");
+  utstring_printf(s, "layout(location = %u) in vec3 inWorldNormal;\n", location);
+  utstring_printf(s, "#endif\n");
   location++;
   utstring_printf(s, "#ifdef IN_TEXCOORD\n");
   utstring_printf(s, "layout(location = %u) in vec2 inTexCoord;\n", location);
