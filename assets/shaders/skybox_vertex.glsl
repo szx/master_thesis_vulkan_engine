0de9f515572c82409dffc21c653690a9986d13a7
Szymon Szczyrbak <szymon.szczyrbak@op.pl>
2022-05-18 18:59:43 +0200

    Fix skybox winding.

--- assets/shaders/skybox_vertex.glsl
+++ assets/shaders/skybox_vertex.glsl
@@ -3,4 +3,3 @@ uint globalIdx = getGlobalIdx();
 outWorldPosition = inPosition;
 mat4 skyboxViewMat = mat4(mat3(global[globalIdx].viewMat)); // Remove translation.
 gl_Position = global[globalIdx].projMat * skyboxViewMat * vec4(inPosition, 1.0); // position in clip space
-gl_Position.y = -gl_Position.y; // HIRO HIRO: Fix winding.
