eb3b0e5ea673a95f4530bb3076e32d7ac180a55f
Szymon Szczyrbak <szymon.szczyrbak@op.pl>
2022-06-29 20:06:46 +0200

    Remove unused macros.

--- src/core/alloc.h
+++ src/core/alloc.h
@@ -40,34 +40,3 @@ int core_memcmp(const void *s1, const void *s2, size_t size);
     }                                                                                              \
     utarray_new(_array, &icd);                                                                     \
   } while (false)
-
-#define core_array(type)                                                                           \
-  struct {                                                                                         \
-    UT_array *array;                                                                               \
-    type *ptr;                                                                                     \
-  }
-
-#define core_array_alloc(name, num)                                                                \
-  do {                                                                                             \
-    UT_icd icd = (UT_icd){sizeof(*((name).ptr)), NULL, NULL, NULL};                                \
-    utarray_new((name).array, &icd);                                                               \
-    utarray_resize((name).array, (num));                                                           \
-    (name).ptr = utarray_front((name).array);                                                      \
-  } while (0)
-
-#define core_array_dealloc(name)                                                                   \
-  do {                                                                                             \
-    utarray_free((name).array);                                                                    \
-  } while (0)
-
-#define core_array_count(name) utarray_len((name).array)
-
-#define _core_array_iter MACRO_CONCAT_LINE(core_array_iter)
-#define core_array_foreach(name, ptrDecl, block)                                                   \
-  do {                                                                                             \
-    for (size_t _core_array_iter = 0; _core_array_iter < utarray_len((name).array);                \
-         ++_core_array_iter) {                                                                     \
-      ptrDecl = &(name).ptr[_core_array_iter];                                                     \
-      block                                                                                        \
-    }                                                                                              \
-  } while (0)
